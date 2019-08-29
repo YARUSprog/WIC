a7609d5abe966c5a021bac1b4889db976dfa3414
Yaroslav Musiienko <yaroslav_musiienko@epam.com>
2019-08-29 18:50:10 +0300

    Disabled https and security

--- src/main/java/com/yarusprog/wic/config/SpringConfig.java
+++ src/main/java/com/yarusprog/wic/config/SpringConfig.java
@@ -4,19 +4,12 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @Configuration
-@EnableWebSecurity
-public class SpringConfig extends WebSecurityConfigurerAdapter {
+//@EnableWebSecurity
+//public class SpringConfig extends WebSecurityConfigurerAdapter {
+public class SpringConfig {
 
     @Bean
     public ModelMapper modelMapper() {
@@ -30,37 +23,37 @@ public class SpringConfig extends WebSecurityConfigurerAdapter {
         return validatorFactoryBean;
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth)
-            throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("user").password("{noop}password").roles("USER")
-                .and()
-                .withUser("admin").password("{noop}password").roles("USER", "ADMIN");
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                //HTTP Basic authentication
-                .httpBasic()
-                .and()
-                .authorizeRequests()
-                .antMatchers(HttpMethod.GET, "/books").hasRole("USER")
-                .antMatchers(HttpMethod.DELETE, "/books/**").hasRole("ADMIN")
-                .and()
-                .csrf().disable()
-                .formLogin().disable();
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        //ok for demo
-        User.UserBuilder users = User.withDefaultPasswordEncoder();
-
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(users.username("user").password("password").roles("USER").build());
-        manager.createUser(users.username("admin").password("password").roles("USER", "ADMIN").build());
-        return manager;
-    }
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth)
+//            throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("user").password("{noop}password").roles("USER")
+//                .and()
+//                .withUser("admin").password("{noop}password").roles("USER", "ADMIN");
+//    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                //HTTP Basic authentication
+//                .httpBasic()
+//                .and()
+//                .authorizeRequests()
+//                .antMatchers(HttpMethod.GET, "/books").hasRole("USER")
+//                .antMatchers(HttpMethod.DELETE, "/books/**").hasRole("ADMIN")
+//                .and()
+//                .csrf().disable()
+//                .formLogin().disable();
+//    }
+//
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        //ok for demo
+//        User.UserBuilder users = User.withDefaultPasswordEncoder();
+//
+//        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+//        manager.createUser(users.username("user").password("password").roles("USER").build());
+//        manager.createUser(users.username("admin").password("password").roles("USER", "ADMIN").build());
+//        return manager;
+//    }
 }
